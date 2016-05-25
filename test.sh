c4487cf3b6f67c3ba40e9098a9061c4c971f971b
norayr <norayr@arnet.am>
2016-05-25 15:04:27 +0400

    small fix.

--- test.sh
+++ test.sh
@@ -16,19 +16,19 @@ LIST=`mysql -uroot -D ${DB} -p -e "SELECT guid FROM wp_posts where post_parent='
 for i in $LIST
 do
   echo $i
-  str0='<a href="'
-  str1='"><img src="'
-  str2="${i%.*}"
-  year=`echo $i | awk -F "/" {' print $7 '}`
-  month=`echo $i | awk -F "/" {' print $8 '}`
-  file=`echo $i | awk -F "/" {' print $9 '}`
-  nfile="${file%.*}"
-  vfile="${nfile}1"
-  sfx=`ls $IMG_PATH/$year/$month/ | grep $nfile | grep -v $vfile | grep 300 | grep -v 150 | awk -F "." {' print $1 '} | awk -F "-" {' print $2 '}`
-  str3='.jpg"/></a><br>'
-  str5="-${sfx}${str3}"
   if [ "$i" != "guid" ]
   then
+    str0='<a href="'
+    str1='"><img src="'
+    str2="${i%.*}"
+    year=`echo $i | awk -F "/" {' print $7 '}`
+    month=`echo $i | awk -F "/" {' print $8 '}`
+    file=`echo $i | awk -F "/" {' print $9 '}`
+    nfile="${file%.*}"
+    vfile="${nfile}1"
+    sfx=`ls $IMG_PATH/$year/$month/ | grep $nfile | grep -v $vfile | grep 300 | grep -v 150 | awk -F "." {' print $1 '} | awk -F "-" {' print $2 '}`
+    str3='.jpg"/></a><br>'
+    str5="-${sfx}${str3}"
     line="${str0}${i}${str1}${str2}${str5}"
     echo $line >> $OUT
   fi
